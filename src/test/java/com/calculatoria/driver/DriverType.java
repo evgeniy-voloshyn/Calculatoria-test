53841c60f6a437805416776b33042093015e2715
Evgeniy Voloshyn <evgeniy.voloshyn@qatestlab.com>
2017-05-30 20:52:08 +0300

    update driver path

--- src/test/java/com/calculatoria/driver/DriverType.java
+++ src/test/java/com/calculatoria/driver/DriverType.java
@@ -2,12 +2,14 @@ package com.calculatoria.driver;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.nio.file.Paths;
+
 
 public enum DriverType implements DriverSetup {
 
     CHROME {
         public DesiredCapabilities getDesiredCapabilities() {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", Paths.get("src/test/resources/drivers/chromedriver.exe").toString());
             return DesiredCapabilities.chrome();
         }
     },
